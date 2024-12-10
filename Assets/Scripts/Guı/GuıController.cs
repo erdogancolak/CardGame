5113a6e9c04b7900d49bd674208503925d0740ad
erd.colak <erd.colak0@gmail.com>
2024-12-10 03:09:24 +0300

    Graphic Options

--- Assets/Scripts/Guı/GuıController.cs
+++ Assets/Scripts/Guı/GuıController.cs
@@ -1,5 +1,7 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GuıController : MonoBehaviour
 {
@@ -7,7 +9,23 @@ public class GuıController : MonoBehaviour
     [SerializeField] private GameObject PlayButtonObject;
     [SerializeField] private GameObject OptionsButtonObject;
     [SerializeField] private GameObject ExitButtonObject;
+    [SerializeField] private GameObject CloseButtonObject;
+    [SerializeField] private GameObject ApplyButtonObject;
+    [SerializeField] private GameObject ResLeftButtonObject;
+    [SerializeField] private GameObject ResRightButtonObject;
     [SerializeField] private GameObject OptionsCanvas;
+    [Space]
+    [SerializeField] private Toggle fullscreenToggle;
+    [SerializeField] private Toggle vsyncToggle;
+    [Space]
+    [SerializeField] private TMP_Text resolutionsText;
+    [SerializeField] private ResItem[] resolutions;
+    private int selectedResolutions;
+    private void Start()
+    {
+        selectedResolutions = 0;
+        UpdateResolutionText();
+    }
     public void PlayButton()
     {
         Animator playAnimator = PlayButtonObject.GetComponent<Animator>();
@@ -26,4 +44,58 @@ public class GuıController : MonoBehaviour
         playAnimator.SetTrigger("Clicked");
         Application.Quit();
     }
+    public void CloseButton()
+    {
+        Animator playAnimator = CloseButtonObject.GetComponent<Animator>();
+        playAnimator.SetTrigger("Clicked");
+        OptionsCanvas.SetActive(false);
+    }
+    public void ApplyButton()
+    {
+        Animator playAnimator = ApplyButtonObject.GetComponent<Animator>();
+        playAnimator.SetTrigger("Clicked");
+        Screen.fullScreen = fullscreenToggle.isOn;
+
+        if(vsyncToggle.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+        Screen.SetResolution(resolutions[selectedResolutions].Horizontal, resolutions[selectedResolutions].Vertical, fullscreenToggle.isOn);
+    }
+    public void ResLeftButton()
+    {
+        Animator playAnimator = ResLeftButtonObject.GetComponent<Animator>();
+        playAnimator.SetTrigger("Clicked");
+        selectedResolutions--;
+        if(selectedResolutions < 0)
+        {
+            selectedResolutions = 0;
+        }
+        UpdateResolutionText();
+    }
+    public void ResRightButton()
+    {
+        Animator playAnimator = ResRightButtonObject.GetComponent<Animator>();
+        playAnimator.SetTrigger("Clicked");
+        selectedResolutions++;
+        if (selectedResolutions > resolutions.Length - 1)
+        {
+            selectedResolutions = resolutions.Length - 1;
+        }
+        UpdateResolutionText();
+    }
+    public void UpdateResolutionText()
+    {
+        resolutionsText.text = resolutions[selectedResolutions].Horizontal.ToString() + " X " +  resolutions[selectedResolutions].Vertical.ToString();
+    }
+}
+[System.Serializable]
+public class ResItem
+{
+    public int Horizontal;
+    public int Vertical;
 }
